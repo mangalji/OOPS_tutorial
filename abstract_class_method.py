09c10230842dc3933eab60f570d9bc1b39bf412c
mangalji <rajmangal03@gmail.com>
2025-12-09 23:32:17 +0530

    abstract class practice file uploaded

---
+++ abstract_class_method.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+	@abstractmethod
+	def area(self):
+		pass
+
+class Rectangle(Shape):
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+	def area(self):
+		return self.width * self.height
+
+class Circle(Shape):
+
+	def __init__(self,radius):
+		self.radius = radius
+
+	def area(self):
+		return 3.14 * self.radius**2
+
+
+r = Rectangle(10,5)
+print("Rectangle: ", r.area())
+
+c = Circle(5)
+print("Circle: ", c.area())
+
